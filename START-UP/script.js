cd35d92bb290f2262126bb45596c5f1ab4e98dac
rikksss <152002620+rikksss@users.noreply.github.com>
2025-01-06 00:08:47 +0800

    Add files via upload

---
+++ START-UP/script.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message! We will get back to you shortly.');
+    this.reset();
+  });
